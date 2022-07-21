be5ef4873779713f37fceccf53688863f18c5fb8
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-07-21 13:04:52 -0300

    resolvendo exercicio 2

---
+++ P2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    float n1, n2, n3, n4, media;
+
+    printf("Digite a primeira nota: \n");
+    printf("Digite a segunda nota: \n");
+    printf("Digite a terceira nota: \n");
+    printf("Digite a quarta nota: \n");
+
+    media = (n1+n2+n3+n4)/4;
+
+    printf("a media foi de: %.2f", media);
+    return(0);
+
+
+}
