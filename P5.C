3c80486500ea7f1a3947ac0ee55b7f84402f1240
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-09-14 19:01:12 -0300

    .

--- P5.C
+++ P5.C
@@ -6,6 +6,14 @@ int mais(){
 
     printf("Informe o valor da base maior: ");
     scanf("%f", &base1);
+    printf("\n Informe o valor da base menor: ");
+    scanf("%f", &base2);
+    printf("\n Informe o valor da altura: ");
+    scanf("%f", &altura);
+
+    area = ((base1 + base2) * altura)/2;
+    printf("\n A area do trapezio e: %.2f", area);
+    return (0);
 
 
 }
